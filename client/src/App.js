e77f2ab6b02a7e6bf5fbe1dded37fc02c992a83a
ezeilo-su <ezeilo.su@gmail.com>
2021-07-11 01:04:07 +0100

    Setup app

---
+++ client/src/App.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
+const App = () => {
+  <Router>
+    <Route path='/chat' component={Chat} />
+  </Router>;
+};
